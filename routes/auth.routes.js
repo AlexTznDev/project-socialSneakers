0452b5decbef450e94d9b2bc718018185c84fd83
AlexTznDev <alex.tuysuzian91@gmail.com>
2023-02-17 14:45:34 +0100

    sign up done

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const User = require("../models/User.model.js")
-
+const bcrypt = require("bcryptjs")
 
 //  GET => "/signup"
 router.get("/signup", (req, res ,next) => {
@@ -12,14 +12,60 @@ router.get("/signup", (req, res ,next) => {
 })
 // POST => "/signup"
 router.post("/signup", async(req, res, next) => {
+
 console.log(req.body)
+
 const {username, email, password} = req.body
+
+if(username === "" || email === "" || password === ""){
+    res.status(401).render("auth/signup-form.hbs", {
+        errorMessage: "All the require are necessary"
+    })
+    return
+}
+
+const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+
+
+if(passwordRegex.test(password)  === false){
+    res.render("auth/signup-form.hbs", {
+        errorMessage: "Password need 8 caracters minimum 1 capital letter and 1 special caracter"
+    });
+    return 
+} 
+
+
 try {
     
+
+const foundUser = await User.findOne({username: username})
+
+
+if(foundUser !== null){
+    res.render("auth/signup-form.hbs", {
+        errorMessage: "username already use"
+    })
+    return
+}
+
+
+const foundUserEmail = await User.findOne({email : email})
+
+if(foundUserEmail !== null){
+    res.render("auth/signup-form.hbs", {
+        errorMessage: "email already use"
+    })
+    return
+}
+const salt = await bcrypt.genSalt(12)
+const hashPassword = await bcrypt.hash (password, salt)
+
+
+
  await User.create({
-    username:username,
-    email:email,
-    password:password
+    username: username,
+    email: email,
+    password: hashPassword
  })
 
 res.redirect("/auth/login")
