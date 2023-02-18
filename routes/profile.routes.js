6bab8b7e423d68474fc33357147dc648397d3c66
AlexTznDev <alex.tuysuzian91@gmail.com>
2023-02-18 21:06:16 +0100

    create post done

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -10,86 +10,86 @@ const uploader = require("../middlewares/cloudinary.js");
 
 // GET => profile routes
 router.get("/", isLoggedIn, async (req, res, next) => {
-    
-    
-    const { _id } = req.session.activeUser;
-    try {
-        // recojer la informacion con el id y enviar a la pagina profil para utilizar la informacion
-        const response = await User.findById(_id)
-        res.render("profile/user-profile.hbs", {
-            userInfo : response
-        });
-        
-    } catch (error) {
-        next(error)
-    }
-  
+  const { _id } = req.session.activeUser;
+  try {
+    // recojer la informacion con el id y enviar a la pagina profil para utilizar la informacion
+    const responseUser = await User.findById(_id);
+    const reponseSneaker = await Sneaker.find({ owner: _id });
+    res.render("profile/user-profile.hbs", {
+      userInfo: responseUser,
+      allSneakers: reponseSneaker
+    });
+
+
+
+  } catch (error) {
+    next(error);
+  }
 });
 
 //POST =>  Formulario en Edit profile container
-router.post("/edit", uploader.single("image") , async (req, res, next) => {
-  
+router.post("/edit", uploader.single("image"), async (req, res, next) => {
   const { age, description } = req.body;
   const { _id } = req.session.activeUser;
 
   // const para cloudinary el url
   let image;
   if (req.file !== undefined) {
-    image = req.file.path
+    image = req.file.path;
   }
 
   try {
-
     // update data from edit profil form
     const response = await User.findByIdAndUpdate(_id, {
       age: age,
       description: description,
-      profilePicture: image // url de cloudinary
+      profilePicture: image, // url de cloudinary
     });
   } catch (error) {}
-  
+
   res.redirect("/profile");
 });
 
-
-
 //POST => Formulario de Create Post
 
-router.post("/postcreate", async (req, res, next) => {
-
-    const { _id } = req.session.activeUser;
-    const { price, forSale, size, color, description, status, brand , model, image } = req.body
-    console.log(forSale)
-    console.log(_id)
-
-    try {
-        
-const response = await Sneaker.create({
-
-    owner: _id,
-    brand:brand,
-    model: model,
-    size: size,
-    color: color,
-    description: description,
-    image: image,
-    status: status,
-    forSale: forSale,
-    price: price
-
+router.post("/postcreate", uploader.single("image"),  async (req, res, next) => {
+  const { _id } = req.session.activeUser;
+  const {
+    price,
+    forSale,
+    size,
+    color,
+    description,
+    status,
+    brand,
+    model,
     
-})
+  } = req.body;
 
 
-    } catch (error) {
-        next(error)
-    }
+  let image;
+  if (req.file !== undefined) {
+    image = req.file.path;
+  }
 
-res.redirect("/profile");
-})
+  try {
+    const response = await Sneaker.create({
+      owner: _id,
+      brand: brand,
+      model: model,
+      size: size,
+      color: color,
+      description: description,
+      image: image,
+      status: status,
+      forSale: forSale,
+      price: price,
+    });
+  } catch (error) {
+    next(error);
+  }
 
-
-
-
+  res.redirect("/profile");
+});
 
 module.exports = router;
