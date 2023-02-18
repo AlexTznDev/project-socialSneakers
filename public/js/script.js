ceeb94304106c9b2bd02899824e9f659386803d7
Victor Vikhal <alfonso.vikhal@gmail.com>
2023-02-18 21:57:20 +0100

    Informacion de post

--- public/js/script.js
+++ public/js/script.js
@@ -8,8 +8,11 @@ document.addEventListener("DOMContentLoaded", () => {
 //  In profile changing style display to edit container and create post
 let editProfileContainerDOM = document.querySelector(".editProfileContainer")
 let createPostContainerDOM = document.querySelector(".createPostContainer")
+let imagePostDOM = document.querySelectorAll(".imagePost")
+let containerVignetUserDOM = document.querySelector(".containerVignetUser")
 let createPostDOM = document.querySelector("#createPost")
 let editProfileDOM = document.querySelector("#editProfile")
+
 
 const changeDisplayEditProfil = () =>{
   editProfileContainerDOM.style.display = "block";
@@ -18,7 +21,16 @@ const changeDisplayCreatPost = () =>{
   createPostContainerDOM.style.display = "block";
 }
 
+const changeDisplayImagePost = () => {
+  containerVignetUserDOM.style.display = "block";
+}
+
+
 
 editProfileDOM.addEventListener('click', changeDisplayEditProfil)
 createPostDOM.addEventListener('click', changeDisplayCreatPost)
+//imagePostDOM.addEventListener('click', changeDisplayImagePost)
+imagePostDOM.forEach((eachOne) =>{
+  eachOne.addEventListener('click', changeDisplayImagePost)
+})
 // *************************************************************************************
