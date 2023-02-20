ce03ff622448fb4d2c6ae62f5e3bef3e0e3e8d24
AlexTznDev <alex.tuysuzian91@gmail.com>
2023-02-20 20:44:46 +0100

    added home page

--- routes/homePage.routes.js
+++ routes/homePage.routes.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
+const Sneaker = require("../models/Sneaker.model.js");
 
 
 
-router.get("/", (req, res, next) => {
+router.get("/",async (req, res, next) => {
+
+try {
+    const allSneakers = await Sneaker.find().select({ _id : 1 })
+    console.log(allSneakers)
+    console.log(allSneakers.length)
+} catch (error) {
+    
+}
+
+
 res.render("homePage/home-page.hbs")
 })
 
