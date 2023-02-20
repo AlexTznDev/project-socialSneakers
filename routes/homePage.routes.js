328d6b7cb258820e02ca7d8b8096096870e7eb31
AlexTznDev <alex.tuysuzian91@gmail.com>
2023-02-21 00:12:03 +0100

    last commit until sleep

--- routes/homePage.routes.js
+++ routes/homePage.routes.js
@@ -3,32 +3,26 @@ const router = express.Router();
 
 const Sneaker = require("../models/Sneaker.model.js");
 
+router.get("/", async (req, res, next) => {
+  try {
+    const allSneakers = await Sneaker.find().select({ _id: 1 , image:1});
+    let arrayOfId = [];
+
+    for (let i = arrayOfId.length; arrayOfId.length < 20; i++) {
+      let randomNumber = Math.floor(Math.random() * allSneakers.length);
+      if (!arrayOfId.includes(allSneakers[randomNumber])) {
+        arrayOfId.push(allSneakers[randomNumber]);
+      }
+    }
+    console.log(arrayOfId);
+
+    res.render("homePage/home-page.hbs", {
+        allId : arrayOfId
+    });
+
+  } catch (error) {}
 
 
-router.get("/",async (req, res, next) => {
+});
 
-try {
-    const allSneakers = await Sneaker.find().select({ _id : 1 })
-    let arrayOfId = []
-
-
-// for(let i=0; allSneakers.){
-
-// }
-
-
-    console.log(allSneakers)
-    console.log(allSneakers.length)
-
-} catch (error) {
-    
-}
-
-
-res.render("homePage/home-page.hbs")
-})
-
-
-
-
-module.exports = router
+module.exports = router;
